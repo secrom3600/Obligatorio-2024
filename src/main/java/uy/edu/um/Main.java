4276d40c206b1ee91d0dbcb802383873ade61018
azael-p <pignanessiazael@gmail.com>
2024-05-28 00:42:28 -0300

    prueba

--- src/main/java/uy/edu/um/Main.java
+++ src/main/java/uy/edu/um/Main.java
@@ -4,7 +4,5 @@ package uy.edu.um;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("hola");
     }
 }
