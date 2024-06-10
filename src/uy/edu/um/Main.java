6e1377f7f9059f86b9d423bb00f576add7f1dcf8
Arturo Cornes <bame36@gmail.com>
2024-06-10 15:03:51 -0300

    Lector y Split del CSV implementado

--- src/uy/edu/um/Main.java
+++ src/uy/edu/um/Main.java
@@ -1,8 +1,13 @@
-package uy.edu.um;
+package src.uy.edu.um;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Funciones misFunciones = new Funciones();
+        misFunciones.cargarMusicas();
+        //funcion creada para ver el correcto funcionamiento
+        misFunciones.listarTodasLasMusicas();
     }
 }
